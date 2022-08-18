0171a14143742de9728ca16e3d31880e83acb804
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-08-18 15:42:43 -0400

    Added Prompts using If Statements

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const generateButton = document.getElementById("generateBtn");
 
-// prompt("How many characters do you want to use? Choose between 8 and 128 characters!");
-// prompt("Do you want to include lowercase?");
-// prompt("Do you want to include lowercase?");
-// prompt("Do you want to include uppercase?");
-// prompt("Do you want to include special characters?");
+function generatePassword() {
+  var initInput = prompt("How many characters do you want to use? Choose between 8 and 128 characters!");
+  var passwordLength = parseInt(initInput);
+
+  if (isNaN(passwordLength)) {
+    prompt("Characters must be between 8 and 128 characters. Try again!");
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    prompt("Characters must be between 8 and 128 characters. Try again!");
+    return
+  }
+
+  if (passwordLength >= 8 || passwordLength <= 128) {
+    prompt("Would you like to include lowercase letters in your password?");
+    return
+  }
+
+
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var symbols = '#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+}
 
 // Write password to the #password input
-function writePassword() {
-  var passwordText = document.querySelector("#password");
-  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 0;
-  var password = "";
+function 
 
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    password += characters.substring(randomNumber, randomNumber +1);
-  }
-   document.getElementById("password").value = password;
-}
+
+
+
+
+
+
+//   for (var i = 0; i <= passwordLength; i++) {
+//     var randomNumber = Math.floor(Math.random() * characters.length);
+//     password += characters.substring(randomNumber, randomNumber +1);
+//   }
+//    document.getElementById("password").value = password;
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  let passwordLength = prompt("How many characters do you want to use? Choose between 8 and 128 characters!");
-  if (passwordLength > 8 || passwordLength < 128)
-  {
-    console.log ("Your new password has " + passwordLength + "characters!")
-  } else {
-    console.log ("Characters must be between 8 and 128 characters. Try again!")
-  }
-} 
+function generatePassword() 
